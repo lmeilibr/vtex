c0e689eec85e9566d5dd13b6ab6d8df69e9eafb5
lmeilibr <leandro.meili@gmail.com>
2019-12-03 00:58:29 -0300

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'vtex-api'
+project = 'vtex'
 copyright = '2019, Leandro Meili'
 author = 'Leandro Meili'
 
@@ -32,6 +32,8 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
