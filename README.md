e893c4b13ebdcea80bddef4c0f90d16a58b80bb2
lmeilibr <leandro.meili@gmail.com>
2019-12-03 01:38:40 -0300

    update README

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 # VTEX
 VTEX API Wrapper for Python
+
+# Example code
+
+You just need to pass your credentials to the Vtex instance,
+and fetch the endpoints.
+
+```python
+from vtex import Vtex
+
+client = Vtex(account_name, environment, app_key, app_token)
+result = client.logistics.get_all_carriers()
+```
+result will be the json response from the get_all_carriers request
--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name='vtex',
-    version='0.1',
+    version='0.1.1',
     license="MIT",
     description='VTEX API Wrapper for Python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Leandro Meili',
     author_email='leandro.meili@gmail.com',
-    packages=['vtex'],
+    packages=find_packages(exclude=("test",)),
     install_requires=['requests'],
+    setup_requires=['wheel'],
 )
