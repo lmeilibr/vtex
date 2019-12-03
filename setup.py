134b8803c30d84f5108029bc0b5494d5e202b5de
lmeilibr <leandro.meili@gmail.com>
2019-12-03 00:26:50 -0300

    add more docs and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vtex',
+    version='0.1',
+    license="MIT",
+    description='VTEX API Wrapper for Python',
+    author='Leandro Meili',
+    author_email='leandro.meili@gmail.com',
+    packages=['vtex'],
+    install_requires=['requests'],
+)
