3494daeeb1433c65a12479482ea2b6dbb7d15a8a
lmeilibr <leandro.meili@gmail.com>
2019-12-03 00:50:43 -0300

    edit conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'Leandro Meili'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
