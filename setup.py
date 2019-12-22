847d04bf6196874a74aeecd1aedc946083cd5c9f
lmeilibr <leandro.meili@gmail.com>
2019-12-21 21:25:10 -0300

    add list_order_by_page function

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ with open('README.md', 'r', 'utf-8') as f:
 
 setup(
     name='vtex',
-    version='0.1.1',
+    version='0.1.2',
     license="MIT",
     description='VTEX API Wrapper for Python',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Leandro Meili',
     author_email='leandro.meili@gmail.com',
+    url='https://github.com/lmeilibr/vtex',
     packages=find_packages(exclude=("test",)),
     install_requires=['requests'],
     setup_requires=['wheel'],
