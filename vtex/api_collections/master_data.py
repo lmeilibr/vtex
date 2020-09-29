b1da83a5e52e51c5d453aad771e685df62544ce0
Eduardo Prjadko <eduardo.prjadko@synapcom.com.br>
2020-09-29 19:28:59 -0300

    added extra functions for data entities and modified 'clients scroll' to 'data entity scroll'

--- vtex/api_collections/master_data.py
+++ vtex/api_collections/master_data.py
@@ -8,7 +8,7 @@ class MasterDataApi(BaseApi):
         return url
 
     def _scroll_url(self):
-        url = f"https://{self.account_name}.vtexcommercestable.com.br/api/dataentities/CL/scroll"
+        url = f"https://{self.account_name}.vtexcommercestable.com.br/api/dataentities/{self.acronym}/scroll"
         return url
 
     def get_profile_by_email(self, email):
@@ -21,11 +21,20 @@ class MasterDataApi(BaseApi):
         url = self._build_url(endpoint)
         return self.get_result(url)
 
-    def get_clients_scroll(self):
+    def get_data_entities_list(self):
+        url = self.base_url + "/api/dataentities/"
+        return self.get_result(url)
+    
+    def get_data_entity_schema(self, acronym):
+        url = self.base_url + f"/api/dataentities/{acronym}/schemas"
+        return self.get_result(url)
+
+    def get_data_entity_scroll(self, acronym):
+        self.acronym = acronym
         url = self._scroll_url() + "?_size=1000"
         result = self.get_result(url)
         return result
 
-    def get_clients_next_scroll(self, token):
+    def get_data_entity_next_scroll(self, token):
         next_url = self._scroll_url() + f"?_token={token}"
         return self.get_result(next_url)
